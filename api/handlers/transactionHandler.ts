dee1c154a1435df5e1f411772a191a2562b98f36
jeffkarr <jkarr001@gmail.com>
2024-07-12 12:26:04 -0500

    aded diagnostic msgs to deposit

--- api/handlers/transactionHandler.ts
+++ api/handlers/transactionHandler.ts
@@ -25,14 +25,18 @@ export const deposit = async (accountID: string, amount: number) => {
   console.dir(account, {depth:null, colors:true});
 
   if (account && amount > 1000.00) {
+    console.log('--- 1 ---');
     throw new Error("Your deposit amount exceeds the transaction limit of $1000");
   }
   else if (account && account.hasOwnProperty('type') && account.type === 'credit' && 
       account.hasOwnProperty('amount') ) {
-    if (amount > account.amount) {
+        console.log('--- 2 ---');
+    if (amount > account.amount) { 
+      console.log('--- 3 ---');
       throw new Error("Credit account deposits cannot exceed your account value");
     }
-  } else {
+  } else { 
+    console.log('--- 4 ---');
     account.amount += amount;
     const res = await query(`
       UPDATE accounts
