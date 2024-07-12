85a74c4a630c0d6e5db2b6b7c218f637caa4f007
jeffkarr <jkarr001@gmail.com>
2024-07-12 17:58:32 -0500

    added useState to UseAlert component

--- ui/src/components/UseAlert.tsx
+++ ui/src/components/UseAlert.tsx
@@ -1,4 +1,5 @@
-import { Stack, Alert } from "@mui/material"
+import React, { useState } from "react";
+import { Stack, Alert } from "@mui/material";
 
 type UseAlertProps = {
     severity: string;
@@ -6,9 +7,11 @@ type UseAlertProps = {
 }
 
 export const UseAlert = ( props: UseAlertProps) => {
+    const [alertSeverity, setAlertSeverity] = useState(props.severity); 
+    const [alertMessage, setAlertMessage] = useState(props.message); 
     return (
         <Stack spacing={2}>
-            <Alert severity={props.severity}>{props.message}</Alert>
+            <Alert severity={alertSeverity}>{alertMessage}</Alert>
         </Stack>
     )
 }
