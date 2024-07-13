6fcbb2e2812258f0dc2a0010c8357eb35d5dce89
jeffkarr <jkarr001@gmail.com>
2024-07-13 15:59:22 -0500

    added closing tag for Alert

--- ui/src/components/AccountDashboard.tsx
+++ ui/src/components/AccountDashboard.tsx
@@ -73,7 +73,7 @@ export const AccountDashboard = (props: AccountDashboardProps) => {
           <Card className="deposit-card">
             <CardContent>
 
-              { useAlert && (<Alert title=null description={useAlertMessage} severity="error" />) }
+              { useAlert && (<Alert title=null description={useAlertMessage} severity="error"></Alert>) }
 
               <h3>Deposit</h3>
               <TextField 
