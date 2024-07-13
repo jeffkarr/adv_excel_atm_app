f59974ca9bbb8016eb34b8702e96090e39f15a90
jeffkarr <jkarr001@gmail.com>
2024-07-13 11:37:00 -0500

    different approach to using props in UseAlert

--- ui/src/components/UseAlert.tsx
+++ ui/src/components/UseAlert.tsx
@@ -1,20 +1,22 @@
 import React, { useState } from "react";
 import { Stack, Alert } from "@mui/material";
 
-type UseAlertProps = {
+interface UseAlertProps {
     severity: string;
     message: string;
 }
 
 export const UseAlert = ( props: UseAlertProps) => {   
-    const [alertMessage, setAlertMessage] = useState(props.message); 
-    const [alertSeverity, setAlertSeverity] = useState(props.severity);
-    if (!alertSeverity) {
-        setAlertSeverity('error');
-    }   
+    // const [alertMessage, setAlertMessage] = useState(props.message); 
+    // const [alertSeverity, setAlertSeverity] = useState(props.severity);
+    // if (!alertSeverity) {
+    //     setAlertSeverity('error');
+    // }   
+
+    const {severity, message} = props;
     return (
         <Stack spacing={2}>
-            <Alert severity={alertSeverity} >{alertMessage}</Alert>
+            <Alert severity={severity}>{message}</Alert>
         </Stack>
     )
 }
