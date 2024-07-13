79a5be509740742a400f13ae92b22ec7ab7a13b4
jeffkarr <jkarr001@gmail.com>
2024-07-13 16:12:35 -0500

    chngd Alert syntax

--- ui/src/components/AccountDashboard.tsx
+++ ui/src/components/AccountDashboard.tsx
@@ -73,7 +73,7 @@ export const AccountDashboard = (props: AccountDashboardProps) => {
           <Card className="deposit-card">
             <CardContent>
 
-              { useAlert && <Alert title=null description={useAlertMessage} severity="error"></Alert> }
+              { useAlert && <Alert title="Alert" description={useAlertMessage} severity="error" /> }
 
               <h3>Deposit</h3>
               <TextField 
