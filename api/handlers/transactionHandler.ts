82cbd0ee5cba4291d3b8cbeb05f0fa06747f3c09
jeffkarr <jkarr001@gmail.com>
2024-07-14 14:33:03 -0500

    revised logic for deposit credit acct scenario

--- api/handlers/transactionHandler.ts
+++ api/handlers/transactionHandler.ts
@@ -24,32 +24,30 @@ export const deposit = async (accountID: string, amount: number) => {
   const account = await getAccount(accountID);
   console.dir(account, {depth:null, colors:true});
 
-  // TEMPORARY TEST CODE 
-  if (account && account.account_number === 1) {
-    account.type = 'credit';
-  }
-  // END OF TEMPORARY TEST CODE
-
   if (account && amount > 1000.00) {
     console.log('--- 1 ---');
     account.restricted = "Your deposit amount exceeds the limit of $1000. Please resubmit a deposit that is below this limit.";
   }
   else if (account && account.hasOwnProperty('type') && account.type === 'credit' && account.hasOwnProperty('amount') ) {
       console.log('--- 2 ---');
-    if (amount > account.amount) { 
-      console.log('--- 3 a ---');
-      account.restricted = "Deposits for credit accounts cannot exceed your account balance. Please resubmit a deposit that is below this limit.";
-    } else {
-      console.log('--- 3 b ---');
-      account.amount += amount;
-      const res = await query(`
-        UPDATE accounts
-        SET amount = $1 
-        WHERE account_number = $2`,
-        [account.amount, accountID]
-      );
-      if (res.rowCount === 0) {
-        throw new Error("Transaction failed");
+    // how much will it take to zero out this credit account balance?
+    if ( account.amount < 0 ) {
+      const amtNeededToZeroAcct = Math.abs(account.amount);
+      if (amount > amtNeededToZeroAcct) {
+        console.log('--- 3 a ---');
+        account.restricted = "Total deposits for a credit account cannot exceed a zero account balance.";
+      } else {
+        console.log('--- 3 b ---');
+        account.amount += amount;
+        const res = await query(`
+          UPDATE accounts
+          SET amount = $1 
+          WHERE account_number = $2`,
+          [account.amount, accountID]
+        );
+        if (res.rowCount === 0) {
+          throw new Error("Transaction failed");
+        }
       }
     }
   } else { 
