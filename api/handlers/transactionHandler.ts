11de817909ec28f65b5bd013d6c745f5639105cf
jeffkarr <jkarr001@gmail.com>
2024-07-14 16:49:40 -0500

    added more deposit logic to hanldle < 0

--- api/handlers/transactionHandler.ts
+++ api/handlers/transactionHandler.ts
@@ -26,34 +26,27 @@ export const deposit = async (accountID: string, amount: number) => {
 
   console.log(`--- amount ${amount}`);
 
-  if (account && amount > 1000.00) {
+  if (amount <= 0 ) {
     console.log('--- 1 ---');
+    account.restricted = "Minimum deposit amount is $1. Please resubmit deposit using a valid dollar amount.";
+  }
+  if (account && amount > 1000.00) {
+    console.log('--- 2 ---');
     account.restricted = "Your deposit amount exceeds the limit of $1000. Please resubmit a deposit that is below this limit.";
   }
-  else if (account && account.hasOwnProperty('type') && account.type === 'credit' && account.hasOwnProperty('amount') ) {
-      console.log('--- 2 ---');
+  if (account && account.hasOwnProperty('type') && account.type === 'credit' && account.hasOwnProperty('amount') ) {
+      console.log('--- 3 ---');
     if ( account.amount <= 0 ) {
       // how much will it take to zero out this credit account balance?
       const amtNeededToZeroAcct = Math.abs(account.amount);
       if (amount > amtNeededToZeroAcct) {
-        console.log('--- 3 a ---');
+        console.log('--- 4 ---');
         account.restricted = "Total deposits for a credit account cannot exceed a zero account balance.";
-      } else {
-        console.log('--- 3 b ---');
-        account.amount += amount;
-        const res = await query(`
-          UPDATE accounts
-          SET amount = $1 
-          WHERE account_number = $2`,
-          [account.amount, accountID]
-        );
-        if (res.rowCount === 0) {
-          throw new Error("Transaction failed");
-        }
-      }
+      } 
     }
-  } else { 
-    console.log('--- 4 ---');
+  } 
+  if ( !account.hasOwnProperty('restricted') ) {
+    console.log('--- 5 ---');
     account.amount += amount;
     const res = await query(`
       UPDATE accounts
