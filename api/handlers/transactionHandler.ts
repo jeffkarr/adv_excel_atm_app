36b0b924b3cc17e6ccdebe677f4c2de28c8bdb33
jeffkarr <jkarr001@gmail.com>
2024-07-14 13:54:24 -0500

    added temp code to test deposit credit scenarios

--- api/handlers/transactionHandler.ts
+++ api/handlers/transactionHandler.ts
@@ -24,16 +24,21 @@ export const deposit = async (accountID: string, amount: number) => {
   const account = await getAccount(accountID);
   console.dir(account, {depth:null, colors:true});
 
+  // TEMPORARY TEST CODE 
+  if (account && account.account_number === 1) {
+    account.type = 'credit';
+  }
+  // END OF TEMPORARY TEST CODE
+
   if (account && amount > 1000.00) {
     console.log('--- 1 ---');
     account.restricted = "Your deposit amount exceeds the limit of $1000. Please resubmit a deposit that is below this limit.";
   }
-  else if (account && account.hasOwnProperty('type') && account.type === 'credit' && 
-      account.hasOwnProperty('amount') ) {
+  else if (account && account.hasOwnProperty('type') && account.type === 'credit' && account.hasOwnProperty('amount') ) {
         console.log('--- 2 ---');
     if (amount > account.amount) { 
       console.log('--- 3 ---');
-      account.restricted = "Credit account deposits cannot exceed your account value";
+      account.restricted = "Deposits for credit accounts cannot exceed your account balance. Please resubmit a deposit that is below this limit.";
     }
   } else { 
     console.log('--- 4 ---');
