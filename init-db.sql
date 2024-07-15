b04c57ffe1100ed995dd2983a1ad2b8aa4b325ea
jeffkarr <jkarr001@gmail.com>
2024-07-15 10:27:23 -0500

    added new account_withdraw_summary table

--- init-db.sql
+++ init-db.sql
@@ -28,3 +28,25 @@ VALUES
     (3, 'Jills Credit', -3000, 'credit', 4000),
     (6, 'Bills Credit', -60000, 'credit', 60000),
     (9, 'Nancy Credit', -90000, 'credit', 100000);
+
+-- CREATE TABLE
+DROP TABLE IF EXISTS account_withdraw_summary;
+CREATE TABLE account_withdraw_summary (
+    account_number INTEGER PRIMARY KEY,
+    withdraw_date DATE NOT NULL,
+    total_amount INTEGER NOT NULL
+);
+
+-- LOAD DATA
+INSERT INTO account_withdraw_summary 
+    (account_number, withdraw_date, total_amount)
+VALUES
+    (1, CURRENT_DATE, 0),
+    (2, CURRENT_DATE, 0),
+    (3, CURRENT_DATE, 0),
+    (4, CURRENT_DATE, 0),
+    (5, CURRENT_DATE, 0),
+    (6, CURRENT_DATE, 0),
+    (7, CURRENT_DATE, 0),
+    (8, CURRENT_DATE, 0),
+    (9, CURRENT_DATE, 0);
